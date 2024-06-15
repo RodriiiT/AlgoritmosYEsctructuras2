275a6420c41615edd321cef49ec0dfa39facde66
Rodrigo <rodrileogabriel@gmail.com>
2024-06-15 18:14:51 -0400

    Se crea un archivo inicial .py

---
+++ Parcial IV Pilas y Colas/GestionTareas.py
@@ -0,0 +1,23 @@
+"""
+Parcial IV. TDA. Listas Enlazadas, Pilas y Colas
+
+Elaborado por:
+
+- Cristian Guevara. C.I: 31.567.525
+- Rodrigo Torres. C.I: 31.014.592
+"""
+
+#1.- Módulo de Gestión de Proyectos
+
+
+
+#2.- Módulo de Gestión de Tareas y Prioridades
+
+
+
+#3.- Módulo de Reportes
+
+
+
+#4.- Módulo de importación y exportación de datos
+
